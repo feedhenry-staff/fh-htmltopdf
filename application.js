26e05ffdbc07b1a37f379d120a9e727e205c435d
Philip Hayes <phayes@PHAYES.local>
2015-02-05 09:16:37 +0000

    Added chmod set executable to linux binary

--- application.js
+++ application.js
@@ -4,6 +4,7 @@ var convert=require("./converter");
 var mbaasApi = require('fh-mbaas-api');
 var mbaasExpress = mbaasApi.mbaasExpress();
 var cors = require('cors');
+var fs = require('fs');
 // Enable CORS for all requests
 app.use(cors());
 
@@ -16,10 +17,18 @@ app.use('/mbaas', mbaasExpress.mbaas);
 app.use(mbaasExpress.fhmiddleware());
 
 app.use("/",express.static(__dirname+"/public"));
+
 app.post("/api",function(req,res){
   res.type("pdf");
   convert(req,res,req.query);
 });
 // allow serving of static files from the public directory
 app.use(express.static(__dirname + '/public'));
-app.listen(process.env.FH_PORT || 8001);
+app.listen(process.env.FH_PORT || 8002);
+
+fs.chmod(__dirname+'/vendors/phantomjs/phantomjs_linux', '550', function(err){
+	if (!err){
+		console.log('File permission changed');
+	}
+
+})
